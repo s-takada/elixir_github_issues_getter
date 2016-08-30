18591193f2e2291690b5f9b475095780707419f5
s0utakada <takada.souta.dev@gmail.com>
2016-08-31 03:30:17 +0900

    [ADD] logger, escript config.

--- mix.exs
+++ mix.exs
@@ -3,11 +3,12 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps]
   end
 
   # Configuration for the OTP application
@@ -31,5 +32,9 @@ defmodule Issues.Mixfile do
       httpoison: "~> 0.8",
       poison:    "~> 1.5"
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
